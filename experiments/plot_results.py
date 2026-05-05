26210ee1cda0548edf2d0c097e0b253c831e14e8
Nandana Dileep <nandanadileep29@gmail.com>
2026-05-05 21:13:01 +0530

    Add queue insight chart for wait time and queue length

--- experiments/plot_results.py
+++ experiments/plot_results.py
@@ -5,6 +5,8 @@ concurrency = [1, 2, 4, 8, 16, 32]
 avg_latency = [0.0777, 0.0997, 0.0932, 0.1614, 0.3008, 0.5249]
 max_latency = [0.0833, 0.1043, 0.0976, 0.1735, 0.3387, 0.6766]
 min_latency = [0.0725, 0.0939, 0.0878, 0.0888, 0.0864, 0.0929]
+avg_wait_time = 0.13621963921377755
+max_queue_length = 28
 
 # Plot 1: Avg Latency vs Concurrency
 plt.figure()
@@ -38,4 +40,13 @@ plt.title("Latency Growth Under Load")
 plt.xscale("log")
 plt.grid()
 plt.savefig("latency_log_scale.png")
+plt.clf()
+
+# Plot 4: Queue insight summary
+plt.figure()
+plt.bar(["avg_wait_time", "max_queue_length"], [avg_wait_time, max_queue_length])
+plt.ylabel("Value")
+plt.title("Queue Insight Summary")
+plt.grid(axis="y")
+plt.savefig("queue_insights.png")
 plt.clf()
